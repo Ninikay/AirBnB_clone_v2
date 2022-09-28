ea1dd85e29142a18b41b49be5a4a15e055743a99
Ninikay <ms.nikeoni@gmail.com>
2022-09-28 14:46:07 +0000

    setup web

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# sets up the web servers for the deployment of web_static
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install nginx
+ mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/defaul
+
+service nginx start
